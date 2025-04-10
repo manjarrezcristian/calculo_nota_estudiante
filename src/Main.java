b6118a4e9f757f73ca0c53d79e79485d03bebc3e
manjarrezcristian <cristiantovarmgmail.com>
2025-04-09 20:19:07 -0500

    estructura inicial del algoritmo para calcular una nota

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        /**desarrollar un sistema de notas para un estudiante que desea saber la nota (de cada materia)
+        el sistema debe pedir el nombre del estudiante documento y programa al que esta matriculado
+                (menu de opciones entre desarrollo de sotware, diseño grafico, gastronomia)
+        debe permitir seleccionar la materia (menu de opciones)
+        desarrollode sotsware: logica, metodologia,introduccion
+        diseño: tipografia,iconografia,semantica
+        gastronomia: costos,panaderia dulce, cocina internacional
+        el calculo de notas es de la siguiente manera,
+        para cada materia hay un examen parcial que vale el 20% de la nota
+        un taller que vale el 30% de la nota y un final que vale el 50% de la nota
+                una vez calculada la nota final tener en cuenta:
+                si la nota esta entre 0 y 2 el estudiante pierde
+                si esta entre 2.1 y 2.9 tiene derecho a plan de mejoramiento
+                si la nota es igual o superior a 3 asi le queda la materia
+                si el estudiante hace plan de mejoramiento la nota maxima sera 3.0
+         el sistema debe discriminar la informacion general del estudiante es decir
+         nombre, documento,nota materia**/
+
+        Scanner
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
